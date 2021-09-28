860a20e3d397d46a83b98ae8dc01b3a486821c03
KUMBARJI Omar <omar.kumbarji@soprasteria.com>
2021-09-28 17:28:36 +0200

    azure stuff

--- src/main/java/de/hsos/masterarbeit/kasse/domain/KafkaTopicConfiguration.java
+++ src/main/java/de/hsos/masterarbeit/kasse/domain/KafkaTopicConfiguration.java
@@ -1,12 +1,17 @@
 package de.hsos.masterarbeit.kasse.domain;
 
+import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class KafkaTopicConfiguration  {
@@ -15,5 +20,15 @@ public class KafkaTopicConfiguration  {
     public NewTopic topicExample() {
         return TopicBuilder.name("articles")
                 .build();
+    }
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "articleservice.servicebus.windows.net:9093");
+        configs.put("sasl.mechanism", "PLAIN");
+        configs.put("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule   required username='$ConnectionString'   password='Endpoint=sb://articleservice.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=rQgOzPQExZtmeAHqx+62v5qZmZfuz5+UfEiwAc1DURI=';");
+        configs.put("security.protocol", "SASL_SSL");
+        return new KafkaAdmin(configs);
     }
 }
